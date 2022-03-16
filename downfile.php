02424a0da293c5a7e03451916a15d05f1b57ad76
Your Name <you@example.com>
2022-03-16 23:55:24 +0700

    Check dang nhap de download

--- downfile.php
+++ downfile.php
@@ -1,25 +1,30 @@
 <?php 
-if(!empty($_GET['file']))
-{
-    $filename = basename($_GET['file']);
-    $filepath = 'upload/' . $filename;
-    if(!empty($filename) && file_exists($filepath)){
-
-        //Define Headers
-        header("Cache-Control: public");
-        header("Content-Description: FIle Transfer");
-        header("Content-Disposition: attachment; filename=$filename");
-        header("Content-Type: application/zip");
-        header("Content-Transfer-Emcoding: binary");
-
-        readfile($filepath);
-        exit;
-
+session_start();
+    if (!(isset($_SESSION['username']) && $_SESSION['username'])) {
+        echo ("<script LANGUAGE='JavaScript'>window.alert('Vui lòng đăng nhập');window.location.href='dangnhap.php';</script>");
     }
     else{
-        echo "This File Does not exist.";
+        if(!empty($_GET['file'])) {
+            $filename = basename($_GET['file']);
+            $filepath = 'upload/' . $filename;
+            if(!empty($filename) && file_exists($filepath)){
+
+                //Define Headers
+                header("Cache-Control: public");
+                header("Content-Description: FIle Transfer");
+                header("Content-Disposition: attachment; filename=$filename");
+                header("Content-Type: application/zip");
+                header("Content-Transfer-Emcoding: binary");
+
+                readfile($filepath);
+                exit;
+
+            }
+            else{
+                echo "This File Does not exist.";
+            }
+        }
     }
-}
 
  ?>
 <html>
